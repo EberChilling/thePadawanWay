dd27106bacc5d6ad2306a3ce89be3c03bf54afb3
Eber Hernández <a01367291@itesm.mx>
2026-03-10 13:10:18 -0600

    Adding an about screen

--- 2_StickerSmash_Expo/app/index.tsx
+++ 2_StickerSmash_Expo/app/index.tsx
@@ -1,9 +1,11 @@
-import { Text, View , StyleSheet} from "react-native";
+import { Text, View , StyleSheet } from "react-native";
+import { Link } from "expo-router" //Allows you to change from one page to another using links
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style= {styles.button}>Go to About Screen</Link>
     </View>
   );
 }
@@ -17,7 +19,10 @@ const styles = StyleSheet.create({
 
   },
 
-  text:{ color: '#fff',
-
+  text:{ color: '#fff'},
+  button:{
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
   },
 });
